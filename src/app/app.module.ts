f9503fbff0a4553e9a0d1d74d26414d757c0ee96
Kevin Schans <kevinvdsd@hotmail.com>
2023-12-13 14:49:19 +0100

    Add general styles to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
+import { DatePipe } from './pipes/date.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
     TodoListItemComponent,
+    DatePipe,
   ],
   imports: [
     BrowserModule
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Todo } from 'src/app/interfaces/todos.interfaces';
 
-
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
