685d5a3ba7d5707be85c627880ce747bd1fb922f
Kevin Schans <kevinvdsd@hotmail.com>
2023-12-13 13:42:48 +0100

    Create todo-list-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoListItemComponent,
   ],
   imports: [
     BrowserModule
